55b0db23428e0dd7c8fe8253f90ef9ab6ece479a
paingkalay7324 <painglay@uw.edu>
2025-10-18 23:18:35 -0700

    fixing base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: '/',
+    base: '/dubHacks25/',
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
